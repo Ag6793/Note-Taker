2fb6434346ed808a0195092e0c7e58f47e504f0f
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-01-16 18:54:30 -0600

    Getting the routes to work

--- Files/public/assets/js/server.js
+++ Files/public/assets/js/server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const path = require('path');
-
+// const api = require ('./index');
 //the GET /api/notes should read the db.json file and return allsaved notes as JSON
 const termData = require('../../../db/db.json');
 const PORT = 3001;
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use('/api', api);
 
 //Middleware to serve static files from /public
 app.use(express.static('public'));
 
-
-//  app.get('/', (req,res) => res.send('Navigate to /notes'));
 //Get * should return the index.html file
  app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '../../../public/index.html'))
  });
 
  //Get /notes should return the notes.html file
+ //When you click "get started" it takes you to notes.html instead of /notes returning the html file???
  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, '../../../public/notes.html'))
  });
 
  //API routes
@@ -33,6 +37,6 @@ app.use(express.static('public'));
 
  //The application should include a db.json file that will be used to store and retrieve notes using the fs module
 
- app.listen(PORT, () => 
-    console.log('Example app listening at http://localhost:${PORT}')
- );
+ app.listen(PORT, () =>
+ console.log(`Example app listening at http://localhost:${PORT}`)
+);
