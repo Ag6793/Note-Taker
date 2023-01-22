5d2bb26d3730068a5064822ce72b01bc51c479a4
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-01-22 13:55:09 -0600

    Debugging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 //Method for generating unique ids
 const uuid = require('./public/assets/js/uuid');
 const noteData = require('./db/db.json');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
